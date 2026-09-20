7fa9e40abdebce05941e00c4daeb4ff98946f76c
Fernando Rojas Oróstica <fernando.rojas@cajalosandes.cl>
2026-09-20 17:44:38 -0300

    Inicializa proyecto Maven y estructura base

---
+++ src/main/java/cl/automatizacion/pruebas/App.java
@@ -0,0 +1,10 @@
+package cl.automatizacion.pruebas;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
